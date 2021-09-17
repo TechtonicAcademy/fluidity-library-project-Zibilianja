5c398d3fc9fe0ca7e551e077df55f27bdf778f98
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-09-17 15:18:41 -0600

    Build router in App. Start replacing anchor tags with link and NavLink.

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,6 @@
-<h2>index</h2>
+import { render } from 'react-dom';
+import ErrorBoundary from './ErrorBoundary';
+import App from './App';
+import './styles/index.scss';
+
+render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
