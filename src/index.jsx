d73d9b8cedbde42aac0db09c48005a2a5d0bfb56
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-09-20 15:31:43 -0600

    Modularize scss more for each header, footer and every page. Create imports for style sheets on each page rather than in index.jsx. Build of bookshelf page.

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import { render } from 'react-dom';
 import React from 'react';
 import ErrorBoundary from './ErrorBoundary';
 import App from './App';
-import './styles/index.scss';
+
 
 render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
