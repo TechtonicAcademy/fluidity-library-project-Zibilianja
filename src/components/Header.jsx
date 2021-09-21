43830469db505ab9f4113a3b2db8baf06f1b82e5
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-09-21 10:46:14 -0600

    Fix header positioning issue, dropdown state implement, book details page built.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import React from 'react';
 import '../styles/header.scss';
+import { useState } from 'react';
 
 const Header = () => {
+    const [dropDown, setDropOpen] = useState(false);
+
 
 const { pathname } = useLocation();
 
@@ -12,14 +15,18 @@ const { pathname } = useLocation();
         <h1 className="header__title">The Library</h1>
         </Link>
         <div className ="dropdown__shown">
-            <button className="click__dropdown"><i className="fa fa-bars header__dropdown"></i></button>
-            <ul className="dropdown__content dropdown--hidden">
+            <button className="click__dropdown" onClick={() => setDropOpen((prev) => !prev)}><i className="fa fa-bars header__dropdown"></i></button>
+            
+            <ul className={dropDown ? 'dropdown--hidden.active' : 'dropdown--hidden'}>
+            <div className="dropdown__content">
                 <li className="dropdown__link dropdown__home"><NavLink to='/' isActive={() => pathname === '/'}>
                     Home
                     </NavLink></li>
                 <li className="dropdown__link dropdown__bookshelf"><NavLink to='/bookshelf'>Bookshelf</NavLink></li>
-                <li className="dropdown__link dropdown__addbook">Add Book</li>
+                <li className="dropdown__link dropdown__addbook"><NavLink to='/addbook'>Add Book</NavLink></li>
+                </div>
             </ul>
+            
         </div>
         <nav className="header__nav"> 
             <ul className="header__list">
