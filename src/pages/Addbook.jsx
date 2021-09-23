9ad1dd77f6132bcdbbeb9ea34994ebe67f00e7f2
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-09-23 13:48:26 -0600

    Beginning build of Add Book form

--- src/pages/Addbook.jsx
+++ src/pages/Addbook.jsx
@@ -1,37 +1,67 @@
 import React from 'react';
+import { useRef } from 'react';
+import { useHistory } from 'react-router';
+import { addBook } from '../utils/API';
 import '../styles/addbook.scss';
 
 const Addbook = () => {
+    const history = useHistory();
+    const titleInput = useRef();
+    const authorInput = useRef();
+    const synopsisInput = useRef();
+    const pageInput = useRef();
+    const publishInput = useRef();
+    const ratingInput = useRef()
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        const title = titleInput.current.value.trim();
+        const author = authorInput.current.value.trim();
+        const synopsis = synopsisInput.current.value.trim();
+        const pages = pageInput.current.value.trim();
+        const published = publishInput.current.value.trim();
+        const rating = ratingInput.current.value.trim();
+
+        if (!title || !author) {
+            return alert('You must include both a title and author to add a book!')
+        }
+
+        addBook({ title, author, synopsis, pages, published, rating })
+            .then((_) => history.pushState('/'))
+            .catch((err) => console.log(err));
+    }
+
   return (
-    <div className="body__addbook">
+     <div className="body__addbook">
       <main className="addbook">
         <h1 className="addbook__title">Add Book</h1>
         <form className="addbook__form">
           <div className="form__left">
             <div className="form__wrappers">
               <label className="form__labels">Title</label>
-              <input type="text" className="form__input form__title" />
+              <input type="text" className="form__input form__title" ref={titleInput}/>
             </div>
             <div className="form__wrappers author__wrapper">
               <label className="form__labels label__author">Author</label>
-              <input type="text" className="form__input form__author" />
+              <input type="text" className="form__input form__author" ref={authorInput}/>
             </div>
             <div className="form__wrappers synopsis__wrapper">
               <label className="form__labels label__synopsis">Synopsis</label>
               <textarea
                 type="text"
                 className="form__input form__synopsis"
+                ref={synopsisInput}
               ></textarea>
             </div>
             <div className="input__smaller">
               <div className="form__wrappers wrappers__sidebyside published__wrapper">
                 <label className="form__labels">Published</label>
-                <input type="text" className="form__input form__published" />
+                <input type="date" className="form__input form__published" ref={publishInput}/>
               </div>
 
               <div className="form__wrappers wrappers__sidebyside pages__wrapper">
                 <label className="form__labels">Pages</label>
-                <input type="number" className="form__input form__pages" />
+                <input type="number" className="form__input form__pages" ref={pageInput}/>
               </div>
             </div>
             <div className="form__wrappers rating__wrapper">
@@ -51,12 +81,10 @@ const Addbook = () => {
             <button className="image__upload">Add Image</button>
           </div>
           <div className="addbook__btnwrap">
-            <input
-              type="button"
-              className="addbook__button button--dark"
-              value="Add Book"
-            />
-            <input type="button" className="addbook__button" value="Cancel" />
+            <button
+              type="submit"
+              className="addbook__button button--dark">Add Book</button>
+            <button className="addbook__button">Cancel</button>
           </div>
         </form>
       </main>
