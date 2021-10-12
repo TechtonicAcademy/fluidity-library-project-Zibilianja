4e23691b09de066bec1fc448a770c2995db3bcdd
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-10-12 15:50:23 -0600

    Update API.js with added variable to store localhost for efficiency.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
+const host = 'http://localhost:8080/'
 
 export const getBooks = () => {
-  return axios.get('http://localhost:8080/api/books');
+  return axios.get(`${host}api/books`);
 };
 
 export const getBook = (id) => {
-  return axios.get(`http://localhost:8080/api/books/${id}`);
+  return axios.get(`${host}api/books/${id}`);
 };
 
 export const editBook = (id, book) => {
-  return axios.put(`http://localhost:8080/api/books/${id}`, book, {
+  return axios.put(`${host}api/books/${id}`, book, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -17,15 +18,15 @@ export const editBook = (id, book) => {
 };
 
 export const searchBooks = (query) => {
-  return axios.get(`http://localhost:8080/api/books/search?query=${query}`);
+  return axios.get(`${host}api/books/search?query=${query}`);
 };
 
 export const deleteBook = (id) => {
-  return axios.delete(`http://localhost:8080/api/books/${id}`);
+  return axios.delete(`${host}api/books/${id}`);
 };
 
 export const addBook = (book) => {
-  return axios.post('http://localhost:8080/api/books/addbook', book, {
+  return axios.post(`${host}api/books/addbook`, book, {
     headers: {
       'Content-Type': 'application/json',
     },
