9ea3e913b86d9b0dfb3eac6f7ea6194a40d1a50a
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-10-13 09:11:23 -0600

    Add /api/books to the variable localhost in API in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-const host = 'http://localhost:8080/'
+const host = 'http://localhost:8080/api/books'
 
 export const getBooks = () => {
-  return axios.get(`${host}api/books`);
+  return axios.get(`${host}`);
 };
 
 export const getBook = (id) => {
-  return axios.get(`${host}api/books/${id}`);
+  return axios.get(`${host}/${id}`);
 };
 
 export const editBook = (id, book) => {
-  return axios.put(`${host}api/books/${id}`, book, {
+  return axios.put(`${host}/${id}`, book, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -18,15 +18,15 @@ export const editBook = (id, book) => {
 };
 
 export const searchBooks = (query) => {
-  return axios.get(`${host}api/books/search?query=${query}`);
+  return axios.get(`${host}/search?query=${query}`);
 };
 
 export const deleteBook = (id) => {
-  return axios.delete(`${host}api/books/${id}`);
+  return axios.delete(`${host}/${id}`);
 };
 
 export const addBook = (book) => {
-  return axios.post(`${host}api/books/addbook`, book, {
+  return axios.post(`${host}/addbook`, book, {
     headers: {
       'Content-Type': 'application/json',
     },
